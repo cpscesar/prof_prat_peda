4360eecfc221ad13a2e4dc33cdfe01bc962abaf6
César P. Soares, PhD <54920630+cpscesar@users.noreply.github.com>
2024-11-13 17:59:14 -0500

    Update teste_modelo_online.py

--- teste_modelo_online.py
+++ teste_modelo_online.py
@@ -52,7 +52,7 @@ model_path = download_model_files()
 
 # Initialize the model and tokenizer using the downloaded files
 tokenizer = BertTokenizer.from_pretrained(model_path)
-model = BertForSequenceClassification.from_pretrained(model_path, num_labels=4, from_safetensors=True)
+model = BertForSequenceClassification.from_pretrained(model_path, num_labels=4)
 model.eval()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
