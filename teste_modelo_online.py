6fc2c3e6922cc0954ad5606ff91cef8f07fc8cd2
César P. Soares, PhD <54920630+cpscesar@users.noreply.github.com>
2024-11-26 11:08:37 -0500

    Update teste_modelo_online.py

--- teste_modelo_online.py
+++ teste_modelo_online.py
@@ -27,6 +27,9 @@ import tempfile
 
 import re
 import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
